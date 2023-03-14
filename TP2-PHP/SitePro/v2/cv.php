22bb02040073084049450add27fb748c0a4ad8e4
josh-bgd <josuaberdugo@gmail.com>
2023-03-14 09:50:08 +0100

    factorisation sitePro v2

---
+++ TP2-PHP/SitePro/v2/cv.php
@@ -0,0 +1,6 @@
+<?php
+    require_once('template_header.php');
+?>
+    <p>cette page contient le cv de Josua</p>
+</body>
+</html>
---
+++ TP2-PHP/SitePro/v2/template_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style1.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../../assets/bootstrap/css/bootstrap.min.css">
+    <link href="../../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../../../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>index</title>
+</head>
+<body>
+<header>
+    <nav class="menu">
+        <ul>
+        <button><li><a href="index.php">Accueil</a></li></button>
+        <button><li><a href="cv.php">CV</a></li></button>
+        <button><li><a href="projets.php">Hobbies</a></li></button>
+        </ul>
+    </nav>
+</header>
