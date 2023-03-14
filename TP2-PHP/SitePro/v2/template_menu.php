094f93a6b92de4888b5506fe63a3e0c8a3ee7e5c
josh-bgd <josuaberdugo@gmail.com>
2023-03-14 14:04:25 +0100

    renderMenuToHTML OK

--- TP2-PHP/SitePro/v2/template_menu.php
+++ TP2-PHP/SitePro/v2/template_menu.php
@@ -1,24 +1,23 @@
 <?php
 function renderMenuToHTML($currentPageId) {
-// un tableau qui d\'efinit la structure du site
-$mymenu = array(
-// idPage titre
-'index' => array( 'Accueil' ),
-'cv' => array( 'Cv' ),
-'projets' => array('Mes Projets')
-);
-// ...
-foreach($mymenu as $pageId => $pageParameters) { 
-    /* On parcourt le tableau $mymenu et la clé de l'élément courant est copiée dans $pageId 
-    tandis que sa valeur est copiée dans $pageparameters*/
-    echo 
-    '<nav class="menu">
-        <ul>
-            <button><li><a href="index.php">Accueil</a></li></button>
-            <button><li><a href="cv.php">CV</a></li></button>
-            <button><li><a href="projets.php">Hobbies</a></li></button>
-        </ul>
-    </nav>'; }
-// ...
+    // un tableau qui d\'efinit la structure du site
+    $mymenu = array(
+    // idPage titre
+        'index' => array( 'Accueil' ),
+        'cv' => array( 'Cv' ),
+        'projets' => array('Mes Projets')
+        );
+    // ...
+    echo '<nav><ul>';
+    foreach ($mymenu as $pageId => $pageParameters) {
+        /* On parcourt le tableau $mymenu et la clé de l'élément courant est copiée dans $pageId 
+        tandis que sa valeur est copiée dans $pageparameters*/
+        if ($pageId == $currentPageId) {
+            echo '<li class="active"><a href="' . $pageId . '.php">' . $pageParameters . '</a></li>';
+        } else {
+            echo '<li><a href="' . $pageId . '.php">' . $pageParameters . '</a></li>';
+        }
+    }
+    echo '</ul></nav>';
 }
 ?>
