c29fd9b9a87c1101f186755685ff2dd7854a91df
josh-bgd <josuaberdugo@gmail.com>
2023-03-14 12:13:20 +0100

    y'a des erreurs mais fin de cours

--- TP2-PHP/SitePro/v2/template_menu.php
+++ TP2-PHP/SitePro/v2/template_menu.php
@@ -1,43 +1,24 @@
 <?php
 function renderMenuToHTML($currentPageId) {
-   
-    // un tableau qui d\'efinit la structure du site
-   /* $mymenu = array(
-        // idPage titre
-        'index' => array( 'Accueil' ),
-        'cv' => array( 'CV' ),
-        'projets' => array('Hobbies')
-    );*/
-
-    // ...
-    if ($currentPageId=="index") {
+// un tableau qui d\'efinit la structure du site
+$mymenu = array(
+// idPage titre
+'index' => array( 'Accueil' ),
+'cv' => array( 'Cv' ),
+'projets' => array('Mes Projets')
+);
+// ...
+foreach($mymenu as $pageId => $pageParameters) { 
+    /* On parcourt le tableau $mymenu et la clé de l'élément courant est copiée dans $pageId 
+    tandis que sa valeur est copiée dans $pageparameters*/
     echo 
     '<nav class="menu">
         <ul>
-            <button><li><a id="selected" href="index.php">Accueil</a></li></button>
+            <button><li><a href="index.php">Accueil</a></li></button>
             <button><li><a href="cv.php">CV</a></li></button>
             <button><li><a href="projets.php">Hobbies</a></li></button>
         </ul>
-    </nav>';
-    } else if ($currentPageId=="cv" ) {
-        echo 
-    '<nav class="menu">
-        <ul>
-            <button><li><a href="index.php">Accueil</a></li></button>
-            <button><li><a id="selected" href="cv.php">CV</a></li></button>
-            <button><li><a href="projets.php">Hobbies</a></li></button>
-        </ul>
-    </nav>';
-    } else {
-        echo 
-        '<nav class="menu">
-            <ul>
-                <button><li><a href="index.php">Accueil</a></li></button>
-                <button><li><a href="cv.php">CV</a></li></button>
-                <button><li><a id="selected" href="projets.php">Hobbies</a></li></button>
-            </ul>
-        </nav>'; 
-    }
-    // ...
+    </nav>'; }
+// ...
 }
 ?>
