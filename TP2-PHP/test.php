020b2688509f316a5ee0ef6f3fb3cd73069025af
josh-bgd <josuaberdugo@gmail.com>
2023-03-15 09:54:16 +0100

    delete index

--- TP2-PHP/test.php
+++ TP2-PHP/test.php
@@ -7,24 +7,48 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    
-        function factorials($t){
-            $taille = count($t);
-            $facts = $t;
-            for ($i=0; $i<$taille; $i++){
-                $facts[$i]=1;
-            } //creation d'un tableau de 1 de même taille que celui en paramètre
-            for($j=0; $j=$taille; $j++){
-                for ($i=1; $i<=$t[$j]; $i++){
-                    $facts[$j]*=$i;
-                }
-            }
-            echo $facts;
-        }
+    <h1>Titre PHP</h1>
+        <?php 
+            $date =  date('h:i:s');
+            echo "il est : $date";
+            echo " le type de l'heure est : ";
+            echo gettype($date);
+            echo " <br>";
+            echo " <br>";
 
-        $tab=[3,5,2,4];
-        factorials($tab);
-    ?>
+            function bonjour($prenom){
+                echo 'Bonjour ' .$prenom. '<br>';
+            }
+            function factorial($n){
+                $fact = 1;
+                for ($i=1; $i<=$n; $i++){
+                    $fact*=$i;
+                }
+                return $fact;
+            }
+            
+            $fact_5= factorial(5);
+            echo "le factoriel de 5 est $fact_5 <br>";
+            bonjour('Josua');
+        ?>
+        <?php
+        
+            function factorials($t){
+                $taille = count($t);
+                $facts = $t;
+                for ($i=0; $i<$taille; $i++){
+                    $facts[$i]=1;
+                } //creation d'un tableau de 1 de même taille que celui en paramètre
+                for($j=0; $j=$taille; $j++){
+                    for ($i=1; $i<=$t[$j]; $i++){
+                        $facts[$j]*=$i;
+                    }
+                }
+                echo $facts;
+            }
+
+            $tab=[3,5,2,4];
+            factorials($tab);
+        ?>
 </body>
 </html>
