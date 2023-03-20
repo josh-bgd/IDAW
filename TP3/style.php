d6193d96e0f1f02901ef52081e6a9e6c8b18be70
josh-bgd <josuaberdugo@gmail.com>
2023-03-20 09:33:00 +0100

    style pas de rafraichissiement

--- TP3/style.php
+++ TP3/style.php
@@ -1,15 +1,18 @@
 <?php
-    $style = 'style1';
-
-    if (isset($_POST['css'])) {
-        $style = $_POST['css'];
-        setcookie('style_cookie', $style, time() + 3600); // stocke la valeur de $style dans un cookie nommé 'style_cookie' pendant 1 heure
-    }
-
+    // Récupération de la valeur du style dans la variable $_GET
+if (isset($_POST['css'])) {
+    $style = $_POST['css'];
+    
+    // Enregistrement de la valeur du style dans un cookie qui expire dans 1 heure
+    setcookie('style_cookie', $style, time() + 3600);
+  } else {
+    // Si la variable $_GET n'est pas définie, on vérifie la présence du cookie
     if (isset($_COOKIE['style_cookie'])) {
-        $style = $_COOKIE['style_cookie'];
+      // Utilisation de la valeur stockée dans le cookie pour définir le style de la page
+      $style = $_COOKIE['style_cookie'];
     } else {
-        $style = 'style1'; // fixe un style par défaut
+      // Si le cookie n'existe pas et que la variable $_GET n'est pas définie, on utilise un style par défaut
+      $style = 'style1';
     }
+  }
 ?>
-
